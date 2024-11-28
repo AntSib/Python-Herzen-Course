a233ca283b5c3735f51da93cc4b9545bbf937cf4
Антон <159721148+AntSib@users.noreply.github.com>
2024-11-28 20:08:40 +0300

    Add files via upload

--- ЛР-5/mongodb_manager.py
+++ ЛР-5/mongodb_manager.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 class MongoDBConnectManager(object):
-    def __init__(self, host="mongodb://localhost:27017/", username="admin", password="admin"):
+    def __init__(self, host="mongodb://localhost", port=27017, username="admin", password="admin"):
         """
         Create a new instance of the MongoDBConnectManager class.
 
@@ -11,6 +11,7 @@ class MongoDBConnectManager(object):
         :return: a MongoDBConnectManager instance
         """
         self.host = host
+        self.port = port
         self.username = username
         self.password = password
         self.connection = None
@@ -22,7 +23,7 @@ class MongoDBConnectManager(object):
         :return: the MongoDBConnectManager instance
         """
         self.connection = MongoClient(
-            self.host,
+            self.host, self.port,
             username=self.username, password=self.password,
             authMechanism="SCRAM-SHA-1"
             )
@@ -34,20 +35,10 @@ class MongoDBConnectManager(object):
 
 
 def main():
-    mongo = MongoDBConnectManager(host="mongodb://localhost:27017/", username="AntSibAdmin", password="AntSibDBPassword")
+    mongo = MongoDBConnectManager(host="mongodb://localhost", port=27017, username="AntSibAdmin", password="AntSibDBPassword")
 
     with mongo as mongo_client:
         users = mongo_client.connection['mynewdb']['user']
-
-        # users_lst = [
-        #     {'name': 'Ada Lovelace', 'age': 205},
-        #     {'name': 'Guide van Rossum', 'age': 67},
-        #     {'name': 'James Gosling', 'age': 69}
-        # ]
-        
-        # users.insert_one({'name': 'Ivan', 'age': 205})
-        # users.insert_many(users_lst)
-        # users.delete_many({})
 
         print(f"All users:")
         found_users = users.find()
@@ -64,19 +55,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Подключение к локальной базе данных MongoDB
-# client = MongoClient('mongodb://localhost:27017/')
-
-# # Выбор базы данных
-# db = client['mydatabase']
-
-# # Работа с коллекцией (таблицей в MongoDB)
-# collection = db['mycollection']
-
-# # Пример вставки документа
-# collection.insert_one({"name": "John", "age": 30})
-
-# # Пример чтения документа
-# document = collection.find_one({"name": "John"})
-# print(document)
