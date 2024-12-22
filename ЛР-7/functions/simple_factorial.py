832fa7b8da8c9323c6cf514d81eea59b4651e829
Антон <159721148+AntSib@users.noreply.github.com>
2024-12-22 21:43:34 +0300

    Update simple_factorial.py

--- ЛР-7/functions/simple_factorial.py
+++ ЛР-7/functions/simple_factorial.py
@@ -1,4 +1,11 @@
 def factorial(n: int) -> int:
+    """
+    Calculates the factorial of a given positive integer n.
+
+    :param n: an integer
+    :return: the factorial of n
+    :raises ValueError: if n is negative or a float
+    """
     if n < 0 or isinstance(n, float):
         raise ValueError("Factorial is not defined for negative numbers or floats")
     result = 1
