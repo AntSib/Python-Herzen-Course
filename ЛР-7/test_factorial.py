bada056cb64b9b4a96fdd906944f4e7351d42da7
Антон <159721148+AntSib@users.noreply.github.com>
2024-12-22 21:37:55 +0300

    Update test_factorial.py

--- ЛР-7/test_factorial.py
+++ ЛР-7/test_factorial.py
@@ -14,12 +14,18 @@ def test_factorial_properties(n):
     if n > 2:
         assert result > n
 
-@given(st.integers(min_value=0, max_value=20))
+@given(st.integers(min_value=0, max_value=100))
 def test_factorial_correctness(n):
     def recursive_factorial(m):
         return 1 if m == 0 else m * recursive_factorial(m - 1)
     
     assert factorial(n) == recursive_factorial(n)
+
+
+@given(st.floats(min_value=-20, max_value=20))
+def test_factorial_float(n):
+    with pytest.raises(ValueError):
+        factorial(n)
 
 
 if __name__ == "__main__":
